46c774943b163f6b7e75281a8f02cce548279601
刘文龙 <liuwenlong@dadingsoft.com>
2019-03-01 14:53:29 +0800

    Changed Application Key to use system variables.

--- PrometheusOneAlert/view.py
+++ PrometheusOneAlert/view.py
@@ -6,33 +6,19 @@ def encrpyt_string(str):
     md = hashlib.md5()
     md.update(str.encode())
     encrpted_str = md.hexdigest()
-    # print encrpted_str
     return encrpted_str
-def hello(request):
-    return HttpResponse("Hello world ! ")
-def send_alert_to_prometheus(request):
-    data = json.loads(request.body)
-    print json.dumps(data, indent=2)
-    print data
-    return HttpResponse('OK')
 def send_alert(request):
     oneAlertAPI = "http://api.onealert.com/alert/api/event"
     headers = {'Content-Type': 'application/json;charset=UTF-8'}
     data = json.loads(request.body)
-    # app = os.getenv('ApplicationKey')
-    app = '5913da62-69b0-ac5a-d0ed-a591e05f800c'
-    print type(data['alerts'][0]['startsAt'])
+    app = os.environ['APPLICATION_KEY']
+    # app = '5913da62-69b0-ac5a-d0ed-a591e05f800c'
     md5_key = str(data['alerts'][0]['startsAt']) + str(data['alerts'][0]['labels']['alertname'])
-    print str(md5_key)
     eventId = str(encrpyt_string(md5_key))
-    print type(eventId)
-    print 'eventId is:\n' + eventId + "\n"
     if data['status'] == 'firing':
         eventType = 'trigger'
     else:
         eventType = 'resolve'
-
-    #eventType = ''
     alarmName = str(data['alerts'][0]['labels']['alertname'])
     entityName = 'PlaceHolder'
     entityId = 'PlaceHolder'
@@ -61,35 +47,10 @@ def send_alert(request):
     }
     print '-------------Data Send to oneAlert-----------\n'
     print json.dumps(content, indent=2)
-    res = requests.request("post",oneAlertAPI,json=content,headers=headers)
+    if len(str(app)) != 0:
+       res = requests.request("post",oneAlertAPI,json=content,headers=headers)
+    else:
+       print "oneAlert ApplicationKey is null!"
     print '-------------Response from oneAlert----------\n'
-    print(res.status_code)
     print(res.text)
     return JsonResponse(res.text,safe=False)
-    # for key,value in data.items():
-    #     print key,value
-    #print json.dumps(data,indent=2)
-    # contexts={
-    #     "receiver": "onealert",
-    #     "status": "firing",
-    #     "alerts": [{
-    #         "status": "firing",
-    #         "labels": {
-    #             "alertname": "ExampleAlert",
-    #             "prometheus": "monitoring/k8s"
-    #         },
-    #         "annotations": {},
-    #         "startsAt": "2019-02-28T08:21:37.99174658Z",
-    #         "endsAt": "0001-01-01T00:00:00Z",
-    #         "generatorURL ": "http: //prometheus-k8s-0:9090/graph?g0.expr=vector%281%29\u0026g0.tab=1"
-    #     }],
-    #     "groupLabels": {},
-    #     "commonLabels": {
-    #         "alertname": "ExampleAlert",
-    #         "prometheus": "monitoring/k8s"
-    #     },
-    #     "commonAnnotations": {},
-    #     "externalURL": "http://alertmanager - main - 0: 9093 ",
-    #     "version ": "4 ",
-    #     "groupKey ": " {}: {}"
-    # }
